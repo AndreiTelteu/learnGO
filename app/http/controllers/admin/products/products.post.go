f58ab0387af87d6edcdcee6f0b096dd06aa771b3
Andrei Telteu <andrei.telteu@touch-media.ro>
2022-10-28 05:28:42 +0300

    Admin post prod, validate prod, create new categories

--- app/http/controllers/admin/products/products.post.go
+++ app/http/controllers/admin/products/products.post.go
@@ -2,13 +2,24 @@ package products
 
 import (
 	"learngo/app/helpers"
+	"learngo/app/helpers/logger"
+	"learngo/app/models"
+	"learngo/database"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 type PostProductRequest struct {
-	Name  string `validate:"required,min=3,max=32"`
-	Email string `validate:"required,email,min=6,max=32"`
+	Name         string                     `validate:"required,min=3,max=32"`
+	Price        float32                    `validate:"omitempty,number"`
+	RegularPrice float32                    `validate:"omitempty,number"`
+	SalePrice    float32                    `validate:"omitempty,number"`
+	Description  string                     `validate:"required,min=3,max=6000"`
+	Categories   []ProductCategoriesRequest `validate:"omitempty,dive"`
+}
+type ProductCategoriesRequest struct {
+	Id   int    `validate:"omitempty,number"`
+	Name string `validate:"omitempty"`
 }
 
 func Post(c *fiber.Ctx) error {
@@ -26,6 +37,31 @@ func Post(c *fiber.Ctx) error {
 			"errors":  errors,
 		})
 	}
+
+	var categories []models.Category
+	if product.Categories != nil && len(product.Categories) > 0 {
+		for _, category := range product.Categories {
+			if category.Id == 0 && len(category.Name) > 0 {
+				// Create new category
+				newCategory := models.Category{
+					Name: category.Name,
+				}
+				database.DB().Create(&newCategory)
+				categories = append(categories, newCategory)
+			}
+			if category.Id > 0 {
+				// Find existing category
+				var existingCategory models.Category
+				result := database.DB().First(&existingCategory, category.Id)
+				if result.RowsAffected > 0 {
+					categories = append(categories, existingCategory)
+				}
+			}
+		}
+	}
+
+	logger.Debug("Final cat list", categories)
+
 	//Return user
-	return c.JSON(product)
+	return c.JSON(categories)
 }
